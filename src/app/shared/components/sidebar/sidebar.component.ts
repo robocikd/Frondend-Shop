f10a3236f356239a7a11ba46c4a22ccc92b92725
Damian <robocikd@wp.pl>
2022-11-16 14:25:05 +0100

    angular material

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  categories = [
+    'Kategoria 1',
+    'Kategoria 2',
+    'Kategoria 3',
+    'Kategoria 4',
+    'Kategoria 5',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
