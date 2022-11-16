211ec3c10837bb039f3bf201a664e543144376a9
Damian <robocikd@wp.pl>
2022-11-16 21:49:53 +0100

    retrieve data from backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DefaultModule } from './layouts/default/default.module';
 import { FullpageModule } from './layouts/fullpage/fullpage.module';
 import { FullpageadminModule } from './layouts/fullpageadmin/fullpageadmin.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     DefaultModule,
     FullpageModule,
     FullpageadminModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -17,6 +17,8 @@ export class ProductComponent implements OnInit {
   }
 
   getProducts() {
-    this.products = this.productService.getProducts();
+    this.productService
+      .getProducts()
+      .subscribe((products) => (this.products = products));
   }
 }
