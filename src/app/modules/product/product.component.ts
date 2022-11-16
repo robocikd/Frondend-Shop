93f1e833ea89737529bcc7cfd900b790114c275a
Damian <robocikd@wp.pl>
2022-11-16 17:00:17 +0100

    product component

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './model/product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts() {
+    this.products = this.productService.getProducts();
+  }
 }
